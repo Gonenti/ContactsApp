7fe6724de2ae25fd02fb8b4bb52826823f6b959b
Bekzod Olimov <Olimov.bekzod.2002@gmail.com>
2023-03-25 14:29:06 +0700

    hello file has been changed

---
+++ src/ContactsApp/ContactsApp.View/MainForm.cs
@@ -0,0 +1,17 @@
+namespace ContactsApp.View
+{
+    public partial class MainForm : Form
+    {
+
+        public MainForm()
+        {   
+            InitializeComponent();
+            
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
